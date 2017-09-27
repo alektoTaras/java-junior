df084f4f0e1dd90b6772a4e6244ff37d4138f740
alektoTaras <32269833+alektoTaras@users.noreply.github.com>
2017-09-28 01:27:52 +0300

    Few changes

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,137 @@
 package com.acme.edu;
 
+import java.io.File;
+
+import static com.acme.edu.Logger.*;
+import static java.io.File.separator;
+import java.io.File;
+import static java.lang.Math.abs;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
+/**
+ * Java Coding Style Convention (PDF)
+ */
 public class Logger {
-    public static void log(int message) {
-        System.out.println("primitive: " + message);
+    public static final String MY_SUPER_CONSTANT = "";
+
+    public static int globalState = 0;
+    public int instanceState = 0;
+
+    public static String ClassName = "";
+    public static final String PRIM= "primitive: ";
+
+    public static void log(byte foramlMessage) {
+        String etalon = (new Byte(foramlMessage)).getClass().toString();
+        if(!ClassName.equals(etalon)) {
+            ClassName = etalon;
+            System.out.println(PRIM+"\r\n" + foramlMessage);
+        }else {
+            System.out.println(foramlMessage);
+        }
+    }
+    public static void log(int foramlMessage) {
+        /*String etalon = (new Integer(foramlMessage)).getClass().toString();
+        if(!ClassName.equals(etalon)) {
+            ClassName = etalon;
+            System.out.println("primitive: \r\n" + foramlMessage);
+        }else {
+            System.out.println(foramlMessage);
+        }*/
+        System.out.print(PRIM + foramlMessage+"\r\n");
+    }
+    public static void log(char foramlMessage) {
+        String etalon = (new Character(foramlMessage)).getClass().toString();
+        if(!ClassName.equals(etalon)) {
+            ClassName = etalon;
+            System.out.println("char: \r\n" + foramlMessage);
+        }else {
+            System.out.println(foramlMessage);
+        }
+    }
+    public static void log(String foramlMessage) {
+        String etalon = (new String(foramlMessage)).getClass().toString();
+        if(!ClassName.equals(etalon)) {
+            ClassName = etalon;
+            System.out.println("string: \r\n" + foramlMessage);
+        }else {
+            System.out.println(foramlMessage);
+        }
     }
 
-    public static void log(byte message) {
+    public static void log(Object obj) {
+        String etalon = obj.getClass().toString();
+        if(!ClassName.equals(etalon)) {
+            ClassName = etalon;
+            System.out.println("reference: \r\n" + '@');
+        }else {
+            System.out.println('@');
+        }
+    }
+
+    public static void log(boolean foramlMessage) {
+        String etalon = new Boolean(foramlMessage).getClass().toString();
+        if(!ClassName.equals(etalon)) {
+            ClassName = etalon;
+            System.out.println("primitive: \r\n" + foramlMessage);
+        }else {
+            System.out.println(foramlMessage);
+        }
+    }
+
+    /**
+     * JavaDoc
+     * <bold>kfdfgjkhdgjfdhg</bold>
+     *
+     * @param message РАПРапрдлпао апдлапорда рдаплро
+     */
+    /*public static void log(byte message) {
         System.out.println("primitive: " + message);
+    }*/
+
+    /**
+     * public API
+     */
+   /* public static void log(String message) {
+        System.out.println(message);
+    }*/
+
+
+    public static void main(String... args) {
+        int local = 0;
+        //kdjfhgkjfgh
+        log(1);
+
+        System.out.println(new String("").getClass().toString());
+//        Main.main();
+//        Main.counter = 1;
     }
 }
+/*
+class Main {
+    public static int counter = 0;
+
+    static {
+        System.out.println("Hello!");
+        counter = 1;
+    }
+
+    public void m() {
+
+    }
+
+    /**
+     * java -Xss2m -Xms4G -Xmx4G
+     */
+/*
+    public static void main(String[] args) {
+        int local = 0;
+        main(new String[] {});
+
+        int factualMessage = 1;
+        Object o = new Object();
+
+        System.out.println(factualMessage);
+    }
+}
+*/
---
+++ src/main/java/demo/PrimitivesDemo.java
@@ -0,0 +1,74 @@
+package demo;
+
+public strictfp class PrimitivesDemo {
+    public strictfp static void main(String[] args) {
+        //region Целые со знаком
+        byte b = 1_00; //1 byte, Byte.MAX_VALUE
+        short s = 010; //2 byte
+        int i = 0x10; //4 bytes
+        long l = 10000000000000L; //8 byte
+        //endregion
+
+        //region С плавающей точкой
+        float f = 0.0000000000000000000000000000000000000001f; //16 byte, IEEE 754?
+        double d = -2e-5d; //32 byte
+        //endregion
+
+        //region Символы
+        char c = '\t'; //\u1234
+        System.out.println(c);
+        //endregion
+
+        //region Логический
+        boolean boo = true | false;
+        //endregion
+        //region Casting
+        int ii=1000;
+        byte bb = (byte) ii;
+        System.out.println(bb);
+        //endregion
+        //region Арифметические
+        int vi=5;
+        double vd=2;
+        System.out.println(vi/vd);//оператор будет возвра0ать ответ в типе старшего типа одного из
+        //операндов
+        System.out.println(1+2+"3"+3+5);//3345
+        //endregion
+
+        //region Shifts
+        int Shi1=5;
+        System.out.println(Shi1>>>1);//разобраться
+        //endregion
+
+        //regioт Тернарный
+        System.out.println(true?"1":"2");
+        //endregion
+
+        //region Проблемы
+        //regioт Type Overflow
+        byte count = 0;
+        for (int j = 0; j < 129; j++) {
+            count ++;
+        }
+        System.out.println(count);
+        //endregion
+        //region Потеря точности
+        System.out.println(.1+.2);
+        //решение: создавать новый класс дробных чисел в
+        //котором будет информация о знаменателе и числителе
+        //с иррациональными числами все ранво будет потеря точности
+
+        //region Деление на 0
+        //Static Analizers:PMD,CheckStyle,FindBugs ||
+        //запускается на этапе сборки
+        System.out.println(1/0);//ArifmeticExeption / by Zero
+        System.out.println(1/0.);//Double.POSITIVE_INFINITY
+        System.out.println(-1/0.);//Double.NEGATIVE_INFINITY
+        System.out.println(0/0.);//Double.NaN
+        //endregion
+        //endregion
+        //endregion
+        System.out.println(1/0.);
+        System.out.println(.1 + .2); //+ type overflow
+    }
+}
